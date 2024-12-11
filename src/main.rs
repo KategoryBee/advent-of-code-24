bb4d856e3ecc2d41b253173927b157f447a163b2
Katherine Broadbent <kategory.bee@gmail.com>
2024-12-11 18:41:15 +1000

    Day 10 part 2

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use std::io;
+use std::{collections::HashMap, io};
 
 use num::Integer;
 
 fn main() {
-    let test_result = solve("test.txt");
-    assert_eq!(test_result, 55312, "test input failed");
-    println!("Test passed");
+    // let test_result = solve("test.txt");
+    // assert_eq!(test_result, 55312, "test input failed");
+    // println!("Test passed");
 
     let result = solve("input.txt");
     println!("result: {result}");
@@ -14,23 +14,32 @@ fn main() {
 fn solve(input_path: &str) -> usize {
     let input = read_input(input_path);
 
+    let mut memoized = HashMap::new();
     let mut total = 0;
-    for stone in input {
-        total += stones_after_iters(&stone, 25);
+    for stone in input.iter() {
+        total += stones_after_iters(&stone, 75, &mut memoized);
     }
 
     total
 }
 
-fn stones_after_iters(stone: &str, iterations_left: i32) -> usize {
+fn stones_after_iters<'a>(
+    stone: &'a str,
+    iterations_left: i32,
+    memoized: &mut HashMap<(&'a str, i32), usize>,
+) -> usize {
+    if let Some(&r) = memoized.get(&(stone, iterations_left)) {
+        return r;
+    }
+
     if iterations_left == 0 {
         return 1;
     }
 
     let iters_remain = iterations_left - 1;
 
-    if stone == "0" {
-        stones_after_iters("1", iters_remain)
+    let res = if stone == "0" {
+        stones_after_iters("1", iters_remain, memoized)
     } else if stone.len().is_even() {
         let mid = stone.len() / 2;
         let (l, r) = stone.split_at(mid);
@@ -38,12 +47,18 @@ fn stones_after_iters(stone: &str, iterations_left: i32) -> usize {
         let r = r.trim_start_matches('0');
         let r = if r.is_empty() { "0" } else { r };
 
-        stones_after_iters(l, iters_remain) + stones_after_iters(r, iters_remain)
+        stones_after_iters(l, iters_remain, memoized)
+            + stones_after_iters(r, iters_remain, memoized)
     } else {
         let mulleed: usize = stone.parse::<usize>().unwrap() * 2024;
 
-        stones_after_iters(&mulleed.to_string(), iters_remain)
-    }
+        // Eh just leak it rather than appease the borrow checker. We're dynamic programming anyway
+        // and have a short runtime.
+        stones_after_iters(mulleed.to_string().leak(), iters_remain, memoized)
+    };
+
+    memoized.insert((stone, iterations_left), res);
+    res
 }
 
 fn read_input(input_path: &str) -> Vec<String> {
