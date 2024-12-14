91618751a8aee42586374572f15b863367b07cd9
Katherine Broadbent <kategory.bee@gmail.com>
2024-12-14 17:00:29 +1000

    Day 13 part 2

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,16 @@ use itertools::Itertools;
 use regex::Regex;
 
 fn main() {
-    let test_result = solve("test.txt");
-    assert_eq!(test_result, 480, "test input failed");
-    println!("Test passed");
+    // No test data available for part 2
+    // let test_result = solve("test.txt");
+    // assert_eq!(test_result, 480, "test input failed");
+    // println!("Test passed");
 
     let result = solve("input.txt");
     println!("result: {result}");
 }
 
-fn solve(input_path: &str) -> i32 {
+fn solve(input_path: &str) -> i64 {
     let input = read_input(input_path);
 
     let mut total = 0;
@@ -24,35 +25,80 @@ fn solve(input_path: &str) -> i32 {
     total
 }
 
-fn solve_puzzle(p: Puzzle) -> i32 {
-    // per spec, the buttons need to be pressed at most 100 times. this is only a space of 10,000
-    // so brute force
+// solving analytically:
 
-    let mut lowest: Option<i32> = None;
+// Puzzle:
+// Button A: X+94, Y+34
+// Button B: X+22, Y+67
+// Prize: X=8400, Y=5400
 
-    for (a, b) in itertools::iproduct!(0..=100, 0..=100) {
-        let pos_x = p.a_x * a + p.b_x * b;
-        let pos_y = p.a_y * a + p.b_y * b;
+// X = 94*a + 22*b
+// Y = 34*a + 67*b
 
-        if pos_x == p.prize_x && pos_y == p.prize_y {
-            let tokens_spent = a * 3 + b;
-            lowest = Some(match lowest {
-                None => tokens_spent,
-                Some(current_min) => current_min.min(tokens_spent),
-            });
-        }
+// substituting in target X and Y:
+// 8400 = 94*a + 22*b
+// 5400 = 34*a + 67*b
+
+// 94a = 8400 - 22b
+// a = (8400 - 22b)/94
+// a = (8400/94) - (22/94)b
+
+// 5400 = 34*a + 67*b
+// 5400 = 34*(8400 - 22b)/94 + 67*b
+// 5400 = 34*(8400/94) - 34(22/94)b + 67*b
+
+// 67*b - 34(22/94)b = 5400 - 34*(8400/94)
+// b(67 - 34(22/94)) = 5400 - 34*(8400/94)
+// b = (5400 - 34*(8400/94))/(67 - 34(22/94))
+// b = 40
+
+// So with placeholders:
+// b = (p_y - a_y*(p_x/a_x))/(b_y - a_y(b_x/a_x))
+// But we need integer solutions only. By the looks of the input data, there's no values for
+// either linear equation that give us anything but 1 solution (aka they're never parallel). So
+// the easy thing is to solve as floating point, round to nearest, and check if the solution works
+// with those integers.
+fn solve_puzzle(p: Puzzle) -> i64 {
+    let a_x = p.a_x as f64;
+    let a_y = p.a_y as f64;
+    let b_x = p.b_x as f64;
+    let b_y = p.b_y as f64;
+    let p_x = p.prize_x as f64;
+    let p_y = p.prize_y as f64;
+
+    let b = (p_y - a_y * (p_x / a_x)) / (b_y - a_y * (b_x / a_x));
+    let a = (p_x - b_x * b) / a_x;
+
+    let a = a.round() as i64;
+    let b = b.round() as i64;
+
+    // Can't have negative button presses. We get these for some invalid solutions. So no
+    // tokens spent.
+    if a < 0 || b < 0 {
+        return 0;
     }
 
-    lowest.unwrap_or(0)
+    // Check found solutions against input. Due to fractions, and our rounding, these may not
+    // actually be valid results. I'm hoping we have enough precision that this check will work in
+    // all cases.
+    let found_x = p.a_x * a + p.b_x * b;
+    let found_y = p.a_y * a + p.b_y * b;
+
+    if found_x != p.prize_x || found_y != p.prize_y {
+        return 0;
+    }
+
+    // Tokens spent:
+    a * 3 + b
 }
 
 struct Puzzle {
-    a_x: i32,
-    a_y: i32,
-    b_x: i32,
-    b_y: i32,
-    prize_x: i32,
-    prize_y: i32,
+    a_x: i64,
+    a_y: i64,
+    b_x: i64,
+    b_y: i64,
+    prize_x: i64,
+    prize_y: i64,
 }
 
 fn read_input(input_path: &str) -> Vec<Puzzle> {
@@ -79,8 +125,8 @@ fn read_input(input_path: &str) -> Vec<Puzzle> {
             a_y: a_y.parse().unwrap(),
             b_x: b_x.parse().unwrap(),
             b_y: b_y.parse().unwrap(),
-            prize_x: prize_x.parse().unwrap(),
-            prize_y: prize_y.parse().unwrap(),
+            prize_x: prize_x.parse::<i64>().unwrap() + 10000000000000,
+            prize_y: prize_y.parse::<i64>().unwrap() + 10000000000000,
         });
     }
 
