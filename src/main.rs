8436c4aed651f026bb29b0060ac271f3ace7387a
Katherine Broadbent <kategory.bee@gmail.com>
2024-12-15 17:21:35 +1000

    Day 15 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,71 @@
 use std::{collections::HashSet, io};
 
-use regex::Regex;
-
 fn main() {
-    // let test_result = solve("test.txt", Vec2(11, 7));
-    // assert_eq!(test_result, 12, "test input failed");
-    // println!("Test passed");
+    let test_result = solve("test.txt");
+    assert_eq!(test_result, 10092, "test input failed");
+    println!("Test passed");
 
-    let result = solve("input.txt", Vec2(101, 103));
+    let result = solve("input.txt");
     println!("result: {result}");
 }
 
-fn solve(input_path: &str, field_size: Vec2) -> i64 {
-    let mut robots = read_input(input_path);
+fn solve(input_path: &str) -> i64 {
+    let mut input = read_input(input_path);
 
-    for tick in 1..1000000 {
-        // Move robots and collect current positions.
-        let mut positions = HashSet::new();
-        for r in robots.iter_mut() {
-            r.pos += r.vel;
+    for m in input.instructions {
+        let dir = m.to_vec();
 
-            r.pos.0 %= field_size.0;
-            if r.pos.0 < 0 {
-                r.pos.0 += field_size.0;
+        // Try to move one dir. Scan in that direction, from the robot, until we hit either an Empty
+        // space, or a wall, or a box. If it's a box, continue scanning.
+        //
+        // If it's a wall, no-op.
+        // Otherwise move robot one cell in wanted direction.
+        // If there's a box there, move that box to the empty space we located to simulate pushing
+        // the entire stock
+        let scan = find_space(input.robot, dir, &input.boxes, &input.walls);
+        match scan {
+            None => (), // found a wall. no op
+            Some(empty_space) => {
+                input.robot += dir;
+                if input.boxes.take(&input.robot).is_some() {
+                    input.boxes.insert(empty_space);
+                }
             }
-            r.pos.1 %= field_size.1;
-            if r.pos.1 < 0 {
-                r.pos.1 += field_size.1;
-            }
-
-            positions.insert(r.pos);
-        }
-
-        if looks_like_an_xmas_tree(&positions) {
-            println!("Iteration {tick}");
-            print_field(&positions, field_size);
-            return tick;
         }
     }
 
-    panic!("Couldn't find tree");
+    gps_sum(&input.boxes)
 }
 
-// Assuming it's a typical xmas tree, it probably has a long uninterrupted vertical bit?
-fn looks_like_an_xmas_tree(positions: &HashSet<Vec2>) -> bool {
-    let expected_len = 10;
-    for p in positions {
-        let mut trunk_len = 0;
-
-        for y in 0..expected_len {
-            if positions.contains(&(*p + Vec2(y, 0))) {
-                trunk_len += 1;
-            }
+fn find_space(
+    start: Vec2,
+    dir: Vec2,
+    boxes: &HashSet<Vec2>,
+    walls: &HashSet<Vec2>,
+) -> Option<Vec2> {
+    let mut pos = start;
+    for _ in 0..1000 {
+        pos += dir;
+        if boxes.contains(&pos) {
+            // Continue scanning
+            continue;
+        }
+        if walls.contains(&pos) {
+            return None;
         }
 
-        if trunk_len == expected_len {
-            return true;
-        }
+        // Otherwise, empty space
+        return Some(pos);
     }
 
-    false
+    panic!("Could not find empty space or wall after too many iterations");
 }
 
-fn print_field(positions: &HashSet<Vec2>, field_size: Vec2) {
-    for y in 0..field_size.1 {
-        for x in 0..field_size.0 {
-            let c = if positions.contains(&Vec2(x, y)) {
-                '#'
-            } else {
-                ' '
-            };
-            print!("{c}");
-        }
-        println!();
-    }
+fn gps_sum(boxes: &HashSet<Vec2>) -> i64 {
+    boxes.iter().map(|b| (100 * b.1 + b.0) as i64).sum()
 }
 
-#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Default, Clone, Copy, PartialEq, Eq, Hash)]
 struct Vec2(i32, i32);
 
 impl std::ops::Add<Vec2> for Vec2 {
@@ -101,25 +90,79 @@ impl std::ops::Mul<i32> for Vec2 {
     }
 }
 
-struct Robot {
-    pos: Vec2,
-    vel: Vec2,
+enum Move {
+    Up,
+    Right,
+    Down,
+    Left,
 }
 
-fn read_input(input_path: &str) -> Vec<Robot> {
-    let mut res = Vec::new();
+impl Move {
+    fn to_vec(&self) -> Vec2 {
+        match self {
+            Move::Up => Vec2(0, -1),
+            Move::Right => Vec2(1, 0),
+            Move::Down => Vec2(0, 1),
+            Move::Left => Vec2(-1, 0),
+        }
+    }
+}
 
-    let pattern = Regex::new(r"p=(-?\d+),(-?\d+) v=(-?\d+),(-?\d+)").unwrap();
+impl From<char> for Move {
+    fn from(value: char) -> Self {
+        match value {
+            '^' => Move::Up,
+            '>' => Move::Right,
+            'v' => Move::Down,
+            '<' => Move::Left,
+            other => panic!("Unknown movement {other}"),
+        }
+    }
+}
+
+#[derive(Default)]
+struct Input {
+    robot: Vec2,
+    walls: HashSet<Vec2>,
+    boxes: HashSet<Vec2>,
+    instructions: Vec<Move>,
+}
+
+fn read_input(input_path: &str) -> Input {
+    let mut res = Input::default();
+
+    let mut y = 0;
 
     for line in read_lines(input_path).unwrap() {
         let line = line.unwrap();
 
-        let (_, [px, py, vx, vy]) = pattern.captures(&line).unwrap().extract();
+        if line.starts_with('#') {
+            for (x, c) in line.chars().enumerate() {
+                let pos = Vec2(x as _, y);
+                match c {
+                    '#' => {
+                        res.walls.insert(pos);
+                    }
+                    'O' => {
+                        res.boxes.insert(pos);
+                    }
+                    '@' => {
+                        res.robot = pos;
+                    }
+                    '.' => (), // empty space
+                    other => panic!("expected map character {other}"),
+                }
+            }
 
-        res.push(Robot {
-            pos: Vec2(px.parse().unwrap(), py.parse().unwrap()),
-            vel: Vec2(vx.parse().unwrap(), vy.parse().unwrap()),
-        });
+            y += 1;
+        } else if line.is_empty() {
+            // No-op
+        } else {
+            // list of instructions
+            for c in line.chars() {
+                res.instructions.push(c.into());
+            }
+        }
     }
 
     res
